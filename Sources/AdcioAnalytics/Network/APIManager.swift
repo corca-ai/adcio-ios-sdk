a9fa27180fc9220e31ed18bb2f3a89d5455afa79
최민재 <cmjcmj1230@gmail.com>
2023-10-27 17:44:32 +0900

    fix: APIMananger divide error case to success case

--- Sources/AdcioAnalytics/Network/APIManager.swift
+++ Sources/AdcioAnalytics/Network/APIManager.swift
@@ -18,22 +18,39 @@ class APIManager {
         headers: HTTPHeaders? = ["Content-Type": "application/json"],
         encoding: ParameterEncoding = JSONEncoding.default
     ) {
-        AF.request(
+        let request = AF.request(
             url,
             method: method,
             parameters: parameters,
             encoding: encoding,
             headers: headers
-        ) {
-            $0.timeoutInterval = 15
-        }.responseJSON { response in
+        )
+        request.responseData { response in
             switch response.result {
-            case.success:q
-                print("\(Date()) - \(url) : 성공")
+            case .success(let data):
+                do {
+                    let resultJSON = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                    if let resultJSON = resultJSON {
+                        if let statusCode = resultJSON["statusCode"] as? Int {
+                            if (300...499).contains(statusCode) {
+                                debugPrint("\(Date()) - \(url) : \(method) Error request - statusCode(\(statusCode))")
+                            } else if (200...299).contains(statusCode) {
+                                debugPrint("\(Date()) - \(url) : \(method) request success - statusCode(\(statusCode))")
+                            }
+                        } else {
+                            if resultJSON.keys.contains("success") {
+                                debugPrint("\(Date()) - \(url) : \(method) request success")
+                            }
+                        }
+                        dump(resultJSON)
+                    }
+                } catch {
+                    debugPrint("Error while decoding response: \(error) from: \(data)")
+                }
             
             case .failure(let error):
+                debugPrint("\(Date()) - \(url) : \(method) Error request")
                 dump(error)
-                break
             }
         }
         
