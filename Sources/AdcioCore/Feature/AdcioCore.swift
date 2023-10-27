a8426cd08047255103e674b4c60a111008cfbc1f
yoohyunmyeong <aahspringaa4@naver.com>
2023-10-27 14:40:31 +0900

    feat: implement adciocore to manage ids

---
+++ Sources/AdcioCore/Feature/AdcioCore.swift
@@ -0,0 +1,59 @@
+import Foundation
+import UIKit
+
+class AdcioCore {
+    
+    public static let shared = AdcioCore()
+
+    private var isInitialized: Bool = false
+    private var clientIdValue: String = ""
+    private var sessionIdValue: String = ""
+    private var deviceIdValue: String = ""
+
+    public func initializeApp(with clientId: String) {
+        self.isInitialized = true
+        self.clientIdValue = clientId
+    }
+    
+    public var clientId: String {
+        get throws {
+            try ensureInitialized()
+            return self.clientIdValue
+        }
+    }
+    
+    public var storeId: String {
+        get throws {
+            try ensureInitialized()
+            return self.clientIdValue
+        }
+    }
+
+    public func sessionId(newSessionId newId: String? = nil) throws -> String {
+        try ensureInitialized()
+        if let newId = newId {
+            sessionIdValue = newId
+        }
+        if sessionIdValue.isEmpty {
+            sessionIdValue = UUID().uuidString
+        }
+        return sessionIdValue
+    }
+    
+    public func deviceId(newDeviceId newId: String? = nil) throws -> String {
+        try ensureInitialized()
+        if let newId = newId {
+            deviceIdValue = newId
+        }
+        if deviceIdValue.isEmpty, let buildId = UIDevice.current.identifierForVendor?.uuidString {
+            deviceIdValue = buildId
+        }
+        return deviceIdValue
+    }
+    
+    private func ensureInitialized() throws {
+        guard isInitialized else {
+            throw SdkError.notInitialized
+        }
+    }
+}
