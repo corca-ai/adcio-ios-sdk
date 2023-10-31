3d41561dd09ecfeaec85486ee6bc3eb762b199af
yoohyunmyeong <aahspringaa4@naver.com>
2023-10-31 18:39:23 +0900

    feat: impl agent provide struct

---
+++ Sources/AdcioAgent/SwiftUI/AdcioAgent.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+import UIKit
+import WebKit
+import AdcioCore
+
+public struct AdcioAgent: UIViewRepresentable {
+    
+    private var baseUrl: String? = "https://agent-dev.adcio.ai"
+    private var clientId: String? = nil
+    private var showAppBar: Bool = false
+    
+    let webView = WKWebView()
+    
+    public init(baseUrl newURL: String = "", clientId: String? = nil, showAppBar: Bool = false) throws {
+        try AdcioCore.shared.clientId
+        self.baseUrl = newURL.isEmpty ? baseUrl : newURL
+        self.clientId = clientId
+        self.showAppBar = showAppBar
+    }
+    
+    public func makeUIView(context: Context) -> WKWebView {
+        let startPage = "start/"
+        let agentURL = try? "\(baseUrl)/\(clientId ?? AdcioCore.shared.clientId)/\(startPage)?platform=ios&show_appbar=\(showAppBar)"
+        print(agentURL)
+        webView.load(URLRequest(url: URL(string: agentURL ?? "")!))
+        return webView
+    }
+    
+    public func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+    }
+}
