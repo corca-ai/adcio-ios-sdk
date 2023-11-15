1d6e99362d8e26df2a1af83fceed7f026d615228
Yu Hyun Myeong <77616769+aahspringaa4@users.noreply.github.com>
2023-11-15 16:03:22 +0900

    fix: remove non logoption entity value (#9)

--- Sources/AdcioAnalytics/Model/AdcioLogOption.swift
+++ Sources/AdcioAnalytics/Model/AdcioLogOption.swift
@@ -8,25 +8,13 @@
 import Foundation
 
 public struct AdcioLogOption {
-    public var sessionId: String
-    public var deviceId: String
-    public var storeId: String
-    public var customerId: String
     public var requestId: String
     public var adsetId: String
     
     public init(
-        sessionId: String,
-        deviceId: String,
-        storeId: String,
-        customerId: String,
         requestId: String,
         adsetId: String
     ) {
-        self.sessionId = sessionId
-        self.deviceId = deviceId
-        self.storeId = storeId
-        self.customerId = customerId
         self.requestId = requestId
         self.adsetId = adsetId
     }
