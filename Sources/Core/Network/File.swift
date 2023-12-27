f081d581d2c6e8a25090c98f05694a417a41e835
yoohyunmyeong <aahspringaa4@naver.com>
2023-12-27 16:32:05 +0900

    feat: add http client core interface

--- Sources/Core/Network/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by 김민식 on 2023/12/27.
-//
-
-import Foundation
---
+++ Sources/Core/Network/HTTPClient.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김민식 on 2023/12/27.
+//
+
+import Foundation
+
+public enum HTTPClientResult {
+    case success(Data, HTTPURLResponse)
+    case failure(Error)
+}
+
+public protocol HTTPClient {
+    func post(from url: URL, parameter: [String: Any], completion: @escaping (HTTPClientResult) -> Void)
+}
