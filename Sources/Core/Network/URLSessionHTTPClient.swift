c9ddcd529a748ac12d63aa2745069e3c832adaed
yoohyunmyeong <aahspringaa4@naver.com>
2023-12-27 16:32:49 +0900

    feat: implement core url session network func

---
+++ Sources/Core/Network/URLSessionHTTPClient.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  
+//
+//  Created by 유현명 on 12/27/23.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {}
+    
+    public func post(from url: URL, parameter: [String: Any], completion: @escaping (HTTPClientResult) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.httpBody = try? JSONSerialization.data(withJSONObject: parameter)
+        
+        session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
