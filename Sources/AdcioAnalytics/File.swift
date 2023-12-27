e0fd8889428c415247463142308964b372a9e465
김민식 <kimminsik@gimminsigs-MacBook-Pro.local>
2023-12-27 11:56:43 +0900

    refactor: delete legacy codes

---
+++ Sources/AdcioAnalytics/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  
+//
+//  Created by 김민식 on 2023/12/21.
+//
+
+import Foundation
+
--- Sources/AdcioCore/Utill/DefaultDeviceId.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-func fetchDefaultDeviceId() -> String {
-    var deviceId: String = ""
-    if let buildId = UIDevice.current.identifierForVendor?.uuidString {
-        deviceId = buildId
-    }
-    return deviceId
-}
