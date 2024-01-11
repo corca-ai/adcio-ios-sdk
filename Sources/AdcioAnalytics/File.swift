1bfc1fbf2c1ef5e3d08b7f7e065d146cb54c0af2
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-11 16:29:43 +0900

    feat: add analytics result

--- Sources/AdcioAnalytics/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  
-//
-//  Created by 김민식 on 2023/12/21.
-//
-
-import Foundation
-
---
+++ Sources/AdcioAnalytics/Model/AnalyticsResult.swift
@@ -0,0 +1,13 @@
+//
+//  AnalyticsResult.swift
+//  
+//
+//  Created by 유현명 on 1/11/24.
+//
+
+import Foundation
+
+public enum AnalyticsResult {
+    case success(Bool)
+    case failure(Error)
+}
