b5afab41932ba84780c663236ae127038514cb4c
yoohyunmyeong <aahspringaa4@naver.com>
2024-01-11 11:34:25 +0900

    feat: add raw data equatable

--- Sources/AdcioPlacement/Model/AdcioSuggestionRawData.swift
+++ Sources/AdcioPlacement/Model/AdcioSuggestionRawData.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  AdcioSuggestionRawData.swift
 //  
 //
 //  Created by 유현명 on 1/8/24.
@@ -7,22 +7,33 @@
 
 import Foundation
 
-public struct AdcioSuggestionRawData: Decodable {
+public struct AdcioSuggestionRawData: Decodable, Equatable {
     public let suggestions: [AdcioSuggestion]
 }
 
-public struct AdcioSuggestion: Decodable {
+public struct AdcioSuggestion: Decodable, Equatable {
     public let logOptions: LogOptions
     public let product: Product
     public let banner: Banner?
+
+    public init(logOptions: LogOptions, product: Product, banner: Banner?) {
+        self.logOptions = logOptions
+        self.product = product
+        self.banner = banner
+    }
 }
 
-public struct LogOptions: Decodable {
+public struct LogOptions: Decodable, Equatable {
     public let requestId: String
     public let adsetId: String
+    
+    public init(requestId: String, adsetId: String) {
+        self.requestId = requestId
+        self.adsetId = adsetId
+    }
 }
 
-public struct Product: Decodable {
+public struct Product: Decodable, Equatable {
     public let id: String
     public let idOnStore: String
     public let storeId: String
@@ -37,9 +48,26 @@ public struct Product: Decodable {
     public let caption: String?
     public let createdAt: String
     public let updatedAt: String
+    
+    public init(id: String, idOnStore: String, storeId: String, sellerId: String, name: String, price: Int, description: String, image: String, includeInRecommendation: Bool, url: String?, deepLink: String?, caption: String?, createdAt: String, updatedAt: String) {
+        self.id = id
+        self.idOnStore = idOnStore
+        self.storeId = storeId
+        self.sellerId = sellerId
+        self.name = name
+        self.price = price
+        self.description = description
+        self.image = image
+        self.includeInRecommendation = includeInRecommendation
+        self.url = url
+        self.deepLink = deepLink
+        self.caption = caption
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+    }
 }
 
-public struct Banner: Decodable {
+public struct Banner: Decodable, Equatable {
     public let id: String
     public let clientId: String
     public let url: String
@@ -51,9 +79,23 @@ public struct Banner: Decodable {
     public let endsAt: String?
     public let createdAt: String
     public let deletedAt: String?
+    
+    public init(id: String, clientId: String, url: String, name: String, categoryId: String?, productId: String?, activated: Bool, startsAt: String, endsAt: String?, createdAt: String, deletedAt: String?) {
+        self.id = id
+        self.clientId = clientId
+        self.url = url
+        self.name = name
+        self.categoryId = categoryId
+        self.productId = productId
+        self.activated = activated
+        self.startsAt = startsAt
+        self.endsAt = endsAt
+        self.createdAt = createdAt
+        self.deletedAt = deletedAt
+    }
 }
 
-public enum Gender: CustomStringConvertible {
+public enum Gender: CustomStringConvertible, Equatable {
     case male
     case female
     
