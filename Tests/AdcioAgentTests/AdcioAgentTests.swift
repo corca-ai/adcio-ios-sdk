7399121f4b1a4288e44d65ad7f6155ecd3867e02
MANSA10004 <mansa@corca.ai>
2024-01-24 15:57:21 +0900

    feat: agent test 코드 추가

--- Tests/AdcioAgentTests/AdcioAgentTests.swift
+++ Tests/AdcioAgentTests/AdcioAgentTests.swift
@@ -1,8 +1,26 @@
 //
-//  File.swift
-//  
+//  AdcioAgentTests.swift
+//
 //
 //  Created by 김민식 on 2023/12/21.
 //
 
-import Foundation
+import XCTest
+import WebKit
+import AdcioAgent
+
+final class AdcioAgentTests: XCTestCase, WKNavigationDelegate {
+    func test_load_requestFromUrl() {
+        let webView = WKWebView(frame: CGRect(x: 0, y: 0, width: 400, height: 400))
+        
+        let url = URL(string: "https://agent.adcio.ai/ABCDE/start/?platform=ios&show_appbar=false")!
+        
+        XCTAssertEqual(webView.estimatedProgress, 0.0)
+        
+        webView.load(URLRequest(url: url))
+        
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            XCTAssertEqual(webView.estimatedProgress, 1.0)
+        }
+    }
+}
