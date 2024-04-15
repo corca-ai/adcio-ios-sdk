c105422c8a568be2c0f4c525696d260bf46136fa
MANSA10004 <mansa@corca.ai>
2024-04-15 16:41:19 +0900

    fix: edit version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Adcio-iOS-SDK",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "AdcioSDK",
                  targets: [
