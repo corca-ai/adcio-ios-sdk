08f8b4db95a79193924789388b81cc89fd0ae4d6
MANSA10004 <mansa@corca.ai>
2024-04-29 15:03:42 +0900

    fixed: edit model

--- Sources/AdcioPlacement/Model/AdvertisementProductRawData.swift
+++ Sources/AdcioPlacement/Model/AdvertisementProductRawData.swift
@@ -8,7 +8,6 @@
 import Foundation
 
 public struct AdcioSuggestionRawData: Decodable, Equatable {
-//    let placement: Placement?
     public let suggestions: [AdcioSuggestion]
 }
 
@@ -42,20 +41,20 @@ public struct Product: Decodable, Equatable {
     public let name: String
     public let price: Int
     public let discountPrice: Int?
-    public let additionalInformation: [AdditionalInformation]?
+//    public let additionalInformation: [AdditionalInformation]?
     public let summary: String
     public let image: String
     public let includeInRecommendation: Bool
     public let url: String?
     public let deepLink: String?
-    public let caption: String?
-    public let additionalImages: [String]?
-    public let decorationImages: [String]?
-    public let createdAt: String
-    public let updatedAt: String
-    public let deletedAt: String?
+//    public let caption: String?
+//    public let additionalImages: [String]?
+//    public let decorationImages: [String]?
+//    public let createdAt: String
+//    public let updatedAt: String
+//    public let deletedAt: String?
     
-    public init(id: String, idOnStore: String, storeId: String, sellerId: String, name: String, price: Int, discountPrice: Int?, additionalInformation: [AdditionalInformation]?, summary: String, image: String, includeInRecommendation: Bool, url: String?, deepLink: String?, caption: String?, additionalImages: [String]?, decorationImages: [String]?, createdAt: String, updatedAt: String, deletedAt: String?) {
+    public init(id: String, idOnStore: String, storeId: String, sellerId: String, name: String, price: Int, discountPrice: Int?, summary: String, image: String, includeInRecommendation: Bool, url: String?, deepLink: String?) {
         self.id = id
         self.idOnStore = idOnStore
         self.storeId = storeId
@@ -63,18 +62,18 @@ public struct Product: Decodable, Equatable {
         self.name = name
         self.price = price
         self.discountPrice = discountPrice
-        self.additionalInformation = additionalInformation
+//        self.additionalInformation = additionalInformation
         self.summary = summary
         self.image = image
         self.includeInRecommendation = includeInRecommendation
         self.url = url
         self.deepLink = deepLink
-        self.caption = caption
-        self.additionalImages = additionalImages
-        self.decorationImages = decorationImages
-        self.createdAt = createdAt
-        self.updatedAt = updatedAt
-        self.deletedAt = deletedAt
+//        self.caption = caption
+//        self.additionalImages = additionalImages
+//        self.decorationImages = decorationImages
+//        self.createdAt = createdAt
+//        self.updatedAt = updatedAt
+//        self.deletedAt = deletedAt
     }
 }
 
