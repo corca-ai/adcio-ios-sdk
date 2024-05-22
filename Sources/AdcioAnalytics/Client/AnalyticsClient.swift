8bc5e3d86a08f30c17034772432ad31cad6a16f4
MANSA10004 <153486944+MANSA10004@users.noreply.github.com>
2024-05-22 14:04:21 +0900

    ADCIO) add sessionId getter interface (#18)
    * fix: edit url components path
    
    * fix: add sessionID getter

--- Sources/AdcioAnalytics/Client/AnalyticsClient.swift
+++ Sources/AdcioAnalytics/Client/AnalyticsClient.swift
@@ -9,6 +9,8 @@ import Foundation
 import Core
 
 public protocol AnalyticsRepogitory {
+    var sessionID: SessionID { get }
+    
     func productTapped(option: AdcioLogOption, completion: @escaping (AnalyticsResult) -> Void)
     func productImpressed(option: AdcioLogOption, completion: @escaping (AnalyticsResult) -> Void)
     func productPurchased(orderID: String, productIDOnStore: String, amount: Int, completion: @escaping (AnalyticsResult) -> Void)
@@ -23,6 +25,7 @@ public class AnalyticsClient: AnalyticsRepogitory {
     private let loader: SessionLoader
     private let deviceID: String
     private let clientID: String
+    public private(set) var sessionID: SessionID
     
     public enum Error: Swift.Error {
         case connectivity
@@ -41,11 +44,12 @@ public class AnalyticsClient: AnalyticsRepogitory {
         self.loader = loader
         self.deviceID = deviceId
         self.baseURL = baseURL
+        self.sessionID = loader.identifier
     }
     
     public func productTapped(option: AdcioLogOption, completion: @escaping (AnalyticsResult) -> Void) {
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = sessionID()
+        parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["storeId"] = clientID
         parameters["customerId"] = nil
@@ -74,7 +78,7 @@ public class AnalyticsClient: AnalyticsRepogitory {
     
     public func productImpressed(option: AdcioLogOption, completion: @escaping (AnalyticsResult) -> Void) {
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = sessionID()
+        parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["storeId"] = clientID
         parameters["customerId"] = nil
@@ -103,7 +107,7 @@ public class AnalyticsClient: AnalyticsRepogitory {
     
     public func productPurchased(orderID: String, productIDOnStore: String, amount: Int, completion: @escaping (AnalyticsResult) -> Void) {
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = sessionID()
+        parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["storeId"] = clientID
         parameters["orderId"] = orderID
@@ -134,7 +138,7 @@ public class AnalyticsClient: AnalyticsRepogitory {
     public func viewChanged(customerID: String? = nil, productIDOnStore: String, title: String? = nil, reqeustID: String? = nil, adsetID: String? = nil, categoryIDOnStore: String? = nil, completion: @escaping (AnalyticsResult) -> Void) {
         
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = sessionID()
+        parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["storeId"] = clientID
         parameters["customerId"] = customerID
@@ -165,7 +169,7 @@ public class AnalyticsClient: AnalyticsRepogitory {
     
     public func addToCart(cartID: String, productIDOnStore: String, completion: @escaping (AnalyticsResult) -> Void) {
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = sessionID()
+        parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["storeId"] = clientID
         parameters["cartId"] = cartID
@@ -190,17 +194,6 @@ public class AnalyticsClient: AnalyticsRepogitory {
                 completion(.failure(AnalyticsClient.Error.connectivity))
             }
         }
-    }
-    
-    // MARK: - Private Inferface
-    private func sessionID() -> SessionID {
-        var sessionID = String()
-        
-        loader.loadSession { id in
-            sessionID = id
-        }
-        
-        return sessionID
     }
     
     private static func map(_ data: Data, from response: HTTPURLResponse) -> AnalyticsResult {
--- Sources/AdcioPlacement/Client/PlacementClient.swift
+++ Sources/AdcioPlacement/Client/PlacementClient.swift
@@ -9,6 +9,8 @@ import Foundation
 import Core
 
 public protocol PlacementRepogitory {
+    var sessionID: SessionID { get }
+    
     func adcioCreateSuggestion(
         clientID: String,
         excludingProductIDs: [String]?,
@@ -21,18 +23,6 @@ public protocol PlacementRepogitory {
         area: String?,
         completion: @escaping (PlacementResult) -> Void
     )
-    
-//    func adcioAdvertisementsBanners(
-//        placementID: String,
-//        customerID: String?,
-//        placementPositionX: Int?,
-//        placementPositionY: Int?,
-//        fromAgent: Bool,
-//        birthYear: Int?,
-//        gender: Gender?,
-//        area: String?,
-//        completion: @escaping (AdvertisementBannerResult) -> Void
-//    )
 }
 
 public final class PlacementClient: PlacementRepogitory {
@@ -40,7 +30,7 @@ public final class PlacementClient: PlacementRepogitory {
     private let client: HTTPClient
     private let loader: SessionLoader
     private let deviceID: String
-    private var sessionID: String?
+    public private(set) var sessionID: SessionID
     
     public init(
         client: HTTPClient = URLSessionHTTPClient(), 
@@ -52,6 +42,7 @@ public final class PlacementClient: PlacementRepogitory {
         self.loader = loader
         self.deviceID = deviceID
         self.baseURL = baseURL
+        self.sessionID = loader.identifier
     }
     
     public enum Error: Swift.Error {
@@ -72,9 +63,6 @@ public final class PlacementClient: PlacementRepogitory {
         completion: @escaping (PlacementResult) -> Void
     ) {
         var parameters: [String : Any] = [:]
-        loader.loadSession { id in
-            sessionID = id
-        }
         parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["placementId"] = placementID
@@ -129,9 +117,6 @@ public final class PlacementClient: PlacementRepogitory {
         completion: @escaping (AdvertisementBannerResult) -> Void
     ) {
         var parameters: [String : Any] = [:]
-        loader.loadSession { id in
-            sessionID = id
-        }
         parameters["sessionId"] = sessionID
         parameters["deviceId"] = deviceID
         parameters["placementId"] = placementID
