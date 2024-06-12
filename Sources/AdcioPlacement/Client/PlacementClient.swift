e70a0a9949b0aa1279bbc5faeacd80da144140e2
MANSA10004 <mansa@corca.ai>
2024-06-12 11:49:58 +0900

    chore: test log

--- Sources/AdcioPlacement/Client/PlacementClient.swift
+++ Sources/AdcioPlacement/Client/PlacementClient.swift
@@ -142,6 +142,9 @@ public final class PlacementClient: PlacementRepogitory {
             ]
         ]}
         
+        print("##", parameters["filters"])
+        print("##", filters?.provinceID.in)
+        
         var components = URLComponents()
         components.scheme = "https"
         components.host = baseURL.absoluteString
