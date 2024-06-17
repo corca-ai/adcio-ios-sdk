c77dc990b43c491aa9e153d17b91e271a54f2d15
MANSA10004 <mansa@corca.ai>
2024-06-17 17:55:08 +0900

    chore: end adcio test

--- Sources/AdcioPlacement/Client/PlacementClient.swift
+++ Sources/AdcioPlacement/Client/PlacementClient.swift
@@ -72,7 +72,7 @@ public final class PlacementClient: PlacementRepogitory {
         client: HTTPClient = URLSessionHTTPClient(),
         loader: SessionLoader = SessionClient.instance,
         deviceID: String = DeviceIDLoader.indentifier,
-        baseURL: URL = URL(string: "api.adcio.ai")!
+        baseURL: URL = URL(string: "api-dev.adcio.ai")!
     ) {
         self.client = client
         self.loader = loader
@@ -202,8 +202,8 @@ public final class PlacementClient: PlacementRepogitory {
         completion: @escaping (PlacementResult) -> Void
     ) {
         var parameters: [String : Any] = [:]
-        parameters["sessionId"] = "hello_world"
-        parameters["deviceId"] = "04af9da9-2c77-4482-8b41-b63e774d4fea"
+        parameters["sessionId"] = sessionID
+        parameters["deviceId"] = deviceID
         parameters["placementId"] = placementID
         parameters["fromAgent"] = fromAgent
         if customerID != nil { parameters["customerId"] = customerID }
@@ -234,12 +234,12 @@ public final class PlacementClient: PlacementRepogitory {
         var components = URLComponents()
         components.scheme = "https"
         components.host = baseURL.absoluteString
-        components.path = "/recommendations/products"
+        components.path = "/v1/recommendations/products"
         
         guard let url = components.url?.absoluteURL else {
             return
         }
-        print(parameters)
+        
         client.request(from: url,
                        parameter: parameters) { result in
             switch result {
