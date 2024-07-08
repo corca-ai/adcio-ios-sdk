cd1898620958bab911f92aefc0dc83391f141c92
MANSA10004 <mansa@corca.ai>
2024-07-08 18:45:09 +0900

    fix: edit sdkVersion

--- Sources/AdcioAnalytics/Domain/AnalyticsManager.swift
+++ Sources/AdcioAnalytics/Domain/AnalyticsManager.swift
@@ -28,7 +28,7 @@ public final class AnalyticsManager: AnalyticsProductManageable, AnalyticsViewMa
     public private(set) var deviceID: String
     private var clientID: String
     private var userAgent: String
-    private let sdkVersion = "iOS 1.2.0"
+    private let sdkVersion = "iOS 1.2.1"
     
     public init(clientID: String) {
         self.client = AnalyticsClient(clientID: clientID)
